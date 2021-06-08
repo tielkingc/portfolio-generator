ae1690b87baa73e5074614dba757986529f3ef7c
tielkingc <tielking5@gmail.com>
2021-06-08 11:09:26 -0400

    added confirm about me

--- app.js
+++ app.js
@@ -39,9 +39,22 @@ const prompyUser = () => {
         }
         },
         {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About Me" section?',
+        default: true
+        },
+        {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself:'
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+            if (confirmAbout) {
+                return true;
+            } else {
+                return false;
+            }
+        }
         }
     ]);
 };
